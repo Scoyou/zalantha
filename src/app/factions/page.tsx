c7b69042f0335f2f9121dcf2e720179dfb482657
Scott Young <youngscottj@gmail.com>
2024-02-04 16:42:26 -0700

    refactor

--- src/app/factions/page.tsx
+++ src/app/factions/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Layout from "../ui/layout";
 import RaceCard from "../ui/race-card";
 import { BackgroundImage } from "../ui/background-image";
--- src/app/ui/race-card.tsx
+++ src/app/ui/race-card.tsx
@@ -1,5 +1,5 @@
 // components/CardComponent.js
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 function RaceCard({
   imageUrl,
