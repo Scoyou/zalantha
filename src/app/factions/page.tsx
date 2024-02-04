dc94ce8a98385fa17df150610a5dd3b641fab440
Scott Young <youngscottj@gmail.com>
2024-02-04 16:32:35 -0700

    refactor

--- src/app/factions/page.tsx
+++ src/app/factions/page.tsx
@@ -10,14 +10,6 @@ export default function Factions() {
         imageSrc="https://d1ta48eu7x3n0t.cloudfront.net/battle_background.webp"
       />
       <div className="items-center text-center">
-        <Image
-          src="https://d1ta48eu7x3n0t.cloudfront.net/zalantha_map.jpeg"
-          alt="A map of Zalantha"
-          className="mx-auto mb-4 lg:w-2/5 lg:h-2/5 object-contain w-80 h-80"
-          width={400}
-          height={400}
-        />
-
         <h1 className="text-2xl font-bold mb-2">
           Races and Factions of Zalantha
         </h1>
