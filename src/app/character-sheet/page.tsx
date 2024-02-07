9bf5e2be3bc836cbefeb194a69a68427be1e08d5
Scott Young <youngscottj@gmail.com>
2024-02-06 19:31:54 -0700

    add character sheet

--- src/app/character-sheet/page.tsx
+++ src/app/character-sheet/page.tsx
@@ -1,15 +1,35 @@
 import Layout from "../ui/layout";
-import { ComingSoon } from "../ui/coming-soon";
 import { BackgroundImage } from "../ui/background-image";
-
+import Image from "next/image";
+import Link from "next/link";
 export default function CharacterSheet() {
   return (
     <Layout>
-      <BackgroundImage
-        altText="Background - a pencil drawing of a castle"
-        imageSrc="https://d1ta48eu7x3n0t.cloudfront.net/character_sheet_background.webp"
-      />
-      <ComingSoon />
+      <div>
+        <BackgroundImage
+          altText="Background - a pencil drawing of a castle"
+          imageSrc="https://d1ta48eu7x3n0t.cloudfront.net/character_sheet_background.webp"
+        />
+        <div className="items-center text-center">
+          <h1 className="text-2xl font-bold mb-2">Character Sheet</h1>
+          <Link
+            href="/charactersheet.pdf"
+            className="inline-block bg-amber-600 hover:bg-brown-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-3 mb-4"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Download
+          </Link>
+        </div>
+        <Image
+          src="https://d1ta48eu7x3n0t.cloudfront.net/character_sheet.jpg"
+          alt="A photo of a scroll"
+          className="mx-auto mb-4 w-screen object-contain"
+          width={400}
+          height={400}
+          priority
+        />
+      </div>
     </Layout>
   );
 }
