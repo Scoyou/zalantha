367d4df0061ba67d3cf03ff9a828a7c3f6e83422
Scott Young <youngscottj@gmail.com>
2024-02-06 21:13:33 -0700

    change download button to image overlay

--- src/app/character-sheet/page.tsx
+++ src/app/character-sheet/page.tsx
@@ -5,30 +5,33 @@ import Link from "next/link";
 export default function CharacterSheet() {
   return (
     <Layout>
-      <div className="min-h-screen">
+      <div className="min-h-screen flex flex-col items-center text-center">
         <BackgroundImage
           altText="Background - a pencil drawing of a castle"
           imageSrc="https://d1ta48eu7x3n0t.cloudfront.net/character_sheet_background.webp"
         />
-        <div className="items-center text-center">
-          <h1 className="text-2xl font-bold mb-2">Character Sheet</h1>
+        <h1 className="text-2xl font-bold mb-2">Character Sheet</h1>
+        <div className="relative inline-block">
           <Link
             href="/charactersheet.pdf"
-            className="inline-block bg-amber-600 hover:bg-brown-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-3 mb-4"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Download
+            <div className="group">
+              <Image
+                src="https://d1ta48eu7x3n0t.cloudfront.net/character_sheet.jpg"
+                alt="A photo of a scroll"
+                className="lg:w-full w-screen object-contain border-4 border-black"
+                priority
+                width={400}
+                height={400}
+              />
+              <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <span className="text-white font-bold">Download</span>
+              </div>
+            </div>
           </Link>
         </div>
-        <Image
-          src="https://d1ta48eu7x3n0t.cloudfront.net/character_sheet.jpg"
-          alt="A photo of a scroll"
-          className="mx-auto mb-4 lg:w-full w-screen object-contain border-4 border-black"
-          priority
-          width={400}
-          height={400}
-        />
       </div>
     </Layout>
   );
