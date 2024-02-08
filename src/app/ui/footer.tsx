b958c9bd7a9d575cbd240deeb5bb98e360fc8195
Scott Young <youngscottj@gmail.com>
2024-02-08 11:45:30 -0700

    update copyright year

--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -2,7 +2,9 @@ export const Footer = () => {
   return (
     <footer className="bg-darkWood border-black border-2 rounded py-3 text-center metamorphous-regular flex flex-col md:flex-row justify-between items-center">
       <div className="md:flex md:items-center">
-        <p className="text-amber-100 md:mb-0">© 2023. All rights reserved.</p>
+        <p className="text-amber-100 md:mb-0">
+          © {new Date().getFullYear()}. All rights reserved.
+        </p>
         <p className="text-xs text-amber-100 md:mb-0 md:ml-2">
           Favicon by{" "}
           <a href="https://icons8.com" className="text-amber-100">
