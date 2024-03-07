675e8f55cdae419ff0e05efbd5531a3ec036b16c
Scott Young <youngscottj@gmail.com>
2024-03-07 13:41:59 -0700

    switch logo to webp

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <header className="relative w-full h-[40rem] overflow-hidden hero-background">
       <Image
-        src={`${process.env.NEXT_PUBLIC_CLOUDFRONT_URL}/logojpg_no_bg.png`}
+        src={`${process.env.NEXT_PUBLIC_CLOUDFRONT_URL}/logojpg_no_bg.webp`}
         layout="fill"
         objectFit="contain"
         objectPosition="center"
